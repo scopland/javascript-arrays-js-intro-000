40b752efce8ea3857c201b74e42043d04a0674cc
Samantha Copland <sam.copland1992@gmail.com>
2020-05-21 18:53:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray (array, element) {return array.unshift (element)}
+function addElementToBeginningOfArray (array, element) 
+{array.unshift (element) return array}
 
 function destructivelyAddElementToBeginningOfArray
